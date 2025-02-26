ca0ccafc59677526fd2c2a9ec46eb07b83df783d
bintangnugraha <djakarta1973@gmail.com>
2025-02-26 08:45:59 +0700

    feat: Add method to filter laundries by user ID

--- backend/app/Http/Controllers/api/LaundryController.php
+++ backend/app/Http/Controllers/api/LaundryController.php
@@ -18,4 +18,23 @@ class LaundryController extends Controller
             'data' => $laundries
         ], 200);
     }
+
+    function whereUserId($id)
+    {
+        $laundries = Laundry::where('user_id', $id)
+            ->with(['shop', 'user'])
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        if ($laundries->isNotEmpty()) {
+            return response()->json([
+                'data' => $laundries,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Not found',
+                'data'    => $laundries,
+            ], 404);
+        }
+    }
 }
