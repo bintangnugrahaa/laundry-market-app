c056b8a5684d13998d6a0fa055394a65c1ee7689
bintangnugraha <djakarta1973@gmail.com>
2025-02-26 08:46:28 +0700

    feat: Add route to filter laundries by user ID

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,6 +16,8 @@ Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/laundry/user/{id}', [LaundryController::class, 'whereUserId']);
+
     Route::get('/promo/limit', [PromoController::class, 'readLimit']);
 
     Route::get('/shop/recommendation/limit', [ShopController::class, 'readRecommendationLimit']);
