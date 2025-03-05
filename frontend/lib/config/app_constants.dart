1115ee642576eeed7886242b07addc14b79d9afb
bintangnugraha <djakarta1973@gmail.com>
2025-03-05 18:10:19 +0700

    feat: add navigation menu for dashboard in AppConstants

--- frontend/lib/config/app_constants.dart
+++ frontend/lib/config/app_constants.dart
@@ -1,3 +1,6 @@
+import 'package:d_view/d_view.dart';
+import 'package:flutter/material.dart';
+
 class AppConstants {
   static const appName = "Star Laundry";
 
@@ -17,5 +20,23 @@ class AppConstants {
     'Ironed',
     'Done',
     'Delivery',
+  ];
+
+  static List<Map> navMenuDashboard = [
+    {
+      'view': DView.empty('Home'),
+      'icon': Icons.home_filled,
+      'label': 'Home',
+    },
+    {
+      'view': DView.empty('My Laundry'),
+      'icon': Icons.local_laundry_service,
+      'label': 'My Laundry',
+    },
+    {
+      'view': DView.empty('Account'),
+      'icon': Icons.account_circle,
+      'label': 'Account',
+    },
   ];
 }
